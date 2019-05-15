a83ccd3d5c845bbbf80b4c429be559b261f072e1
khyreerusydi <nicoraventbh@gmail.com>
2019-05-15 14:54:30 +0800

    render list of saved todo items

--- script.jsx
+++ script.jsx
@@ -12,26 +12,30 @@ class List extends React.Component {
     changeHandler = (event) => {
         this.setState({word:event.target.value});
         console.log("change", event.target.value);
-
     }
 
     submitHandler = (event) => {
         console.log("word", this.state.word);
         console.log("list", this.state.list);
+
         let clearWord = "";
         let updatedList = this.state.list;
         updatedList.push(this.state.word);
+
         this.setState({word: clearWord, list: updatedList});
     }
 
     render() {
     // render the list with a map() here
-
         console.log("rendering");
         return (
             <div className="list">
                 <input onChange={this.changeHandler} value={this.state.word}/>
                 <button onClick={this.submitHandler}>add item</button>
+                <br/>
+                <ul>
+                    {this.state.list.map((item, index) => <li key={index}>{item}</li>)}
+                </ul>
             </div>
         );
     }
