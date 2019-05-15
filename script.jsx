2a4cbf8da55fa3ef5daec544ce3ddc51bc6fe45d
khyreerusydi <nicoraventbh@gmail.com>
2019-05-15 15:05:17 +0800

    Created a length validation on the input

--- script.jsx
+++ script.jsx
@@ -11,18 +11,27 @@ class List extends React.Component {
 
     changeHandler = (event) => {
         this.setState({word:event.target.value});
-        console.log("change", event.target.value);
+        console.log("change", event.target.value.length);
     }
 
     submitHandler = (event) => {
-        console.log("word", this.state.word);
+        console.log("word", this.state.word.length);
         console.log("list", this.state.list);
 
         let clearWord = "";
         let updatedList = this.state.list;
-        updatedList.push(this.state.word);
 
-        this.setState({word: clearWord, list: updatedList});
+        if (this.state.word.length < 1) {
+            alert("Please enter a todo item");
+        }
+        else if (this.state.word.length > 200){
+            alert("Your todo item should be less than 200 characters")
+        }
+        else {
+            updatedList.push(this.state.word);
+            this.setState({word: clearWord, list: updatedList});
+        }
+
     }
 
     render() {
