066324957db6a37814c7fa0933ca44b161c4669f
khyreerusydi <nicoraventbh@gmail.com>
2019-05-16 08:52:00 +0800

    more stylings

--- script.jsx
+++ script.jsx
@@ -64,7 +64,7 @@ class List extends React.Component {
                 <td><EditableLabel value={item[0]}/></td>
                 <td>{item[1]}</td>
                 <td>
-                    <button className="removeButton" onClick={() => this.deleteHandler(index)}>remove item</button>
+                    <button className="removeButton" onClick={() => this.deleteHandler(index)}>remove</button>
                 </td>
             </tr>
         )
@@ -180,6 +180,87 @@ class EditableLabel extends React.Component {
     }
 }
 
+class DragDrop extends React.Component {
+
+    state = {
+        tasks: [
+          {id: "1", taskName:"Read book",type:"inProgress", backgroundColor: "red"},
+          {id: "2", taskName:"Pay bills", type:"inProgress", backgroundColor:"green"},
+          {id: "3", taskName:"Go to the gym", type:"Done", backgroundColor:"blue"},
+          {id: "4", taskName:"Play baseball", type:"Done", backgroundColor:"green"}
+        ]
+    }
+
+    onDragStart = (event, taskName) => {
+        console.log('dragstart on div: ', taskName);
+        event.dataTransfer.setData("taskName", taskName);
+    }
+
+    onDragOver = (event) => {
+        event.preventDefault();
+    }
+
+    onDrop = (event, cat) => {
+        let taskName = event.dataTransfer.getData("taskName");
+
+        let tasks = this.state.tasks.filter((task) => {
+            if (task.taskName == taskName) {
+                task.type = cat;
+            }
+            return task;
+        });
+
+        this.setState({
+            ...this.state,
+            tasks
+        });
+    }
+
+    render() {
+
+        var tasks = {
+          inProgress: [],
+          Done: []
+        }
+
+        this.state.tasks.forEach ((task) => {
+          tasks[task.type].push(
+            <div key={task.id}
+              onDragStart = {(event) => this.onDragStart(event, task.taskName)}
+              draggable
+              className="draggable"
+              style = {{backgroundColor: task.bgcolor}}>
+              {task.taskName}
+            </div>
+          );
+        });
+
+        return (
+          <div className="drag-container">
+            <h2 className="head">To Do List Drag & Drop</h2>
+            <div className="inProgress"
+                onDragOver={(event)=>this.onDragOver(event)}
+                onDrop={(event)=>{this.onDrop(event, "inProgress")}}>
+              <span className="group-header">In Progress</span>
+              {tasks.inProgress}
+            </div>
+            <div className="droppable"
+                onDragOver={(event)=>this.onDragOver(event)}
+                onDrop={(event)=>this.onDrop(event, "Done")}>
+                <span className="group-header">Done</span>
+                {tasks.Done}
+            </div>
+            <div className="inProgress"
+                onDragOver={(event)=>this.onDragOver(event)}
+                onDrop={(event)=>{this.onDrop(event, "inProgress")}}>
+                <span className="group-header">In Progress</span>
+                {tasks.inProgress}
+            </div>
+          </div>
+        );
+    }
+}
+
 ReactDOM.render(
     <div>
         <Clock/>
@@ -187,5 +268,3 @@ ReactDOM.render(
     </div>,
     document.getElementById('root')
 );
-
-// <button onClick={() => this.submitHandler(event)}>add item</button>
